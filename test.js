f789b200843f3b9727f9611b4f11a20fd0d32966
Ian Sinnott <ian@iansinnott.com>
2020-11-23 21:19:35 +0800

    Add simplified

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const { isChinese, hasChinese } = require("./index.js");
 
 it("Should work on a full chinese sentence", () => {
-  expect(isChinese("你好嗎？還有誰要來？  我放幾個空格看看。")).toBe(true);
+  expect(isChinese("你好嗎？還有誰要來？  我放幾個空格看看。")).toBe(true); // 繁體
+  expect(isChinese("你好吗？还有谁要来？  我放几个空格看看。")).toBe(true); // 檢體
 });
 
 it("Should work on a full chinese sentence", () => {
